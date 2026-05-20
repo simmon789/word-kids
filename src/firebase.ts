77f8a9e41d2eddc0aa2d60e8d56e3face3feafcc
เบอร์มงคล ทำนายเบอร์ เช็คเบอร์มงคล เบอร์โทรเลขมงคล <67509546+simmon789@users.noreply.github.com>
2026-05-20 17:19:37 +0700

    Add Firebase Storage initialization
    add firebase storage export

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBayp0T_Dq_HC8abs21Yc7Cesi8JPELIUA",
@@ -8,10 +9,11 @@ const firebaseConfig = {
   projectId: "word-star-kids",
   storageBucket: "word-star-kids.firebasestorage.app",
   messagingSenderId: "818510968906",
-  appId: "1:818510968906:web:4d4e47ed94b4ec2f738eb3"
+  appId: "1:818510968906:web:4d4e47ed94b4ec2f738eb3",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
